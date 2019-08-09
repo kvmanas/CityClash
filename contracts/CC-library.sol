d595657273d8e8a60d2aa5b193c8e0d35c95feff
Anas KVM <kvmanas@gmail.com>
2019-08-09 16:49:22 +0530

    remove express

--- contracts/CC-library.sol
+++ contracts/CC-library.sol
@@ -24,16 +24,37 @@ library CClibrary{
         mapping(address => PlayerModel) Players;
         MarketOrders[] SellOrders;
     }
+    /**
+    * Get Player Town details as array
+    * @param self Game
+    * @return user town address
+    */
     function getPlayerTowns(CCModel storage self) public view returns(address[] memory){
         return self.Players[msg.sender].Towns;
     }
+     /**
+    * Substract Gem from user
+    * @param self Game
+    * @param _amount Gem amount to be reduced
+    */
     function subThisPlayerGems(CCModel storage self, uint256 _amount) internal{
         require(self.Players[msg.sender].GemsCount >= _amount, "insufficient gems");
         self.Players[msg.sender].GemsCount = self.Players[msg.sender].GemsCount.sub(_amount);
     }
+    /**
+    * Add Gem to user
+    * @param self Game
+    * @param _user user address
+    * @param _amount Gem amount to be reduced
+    */
     function addPlayerGems(CCModel storage self, address _user, uint256 _amount) internal {
         self.Players[_user].GemsCount = self.Players[_user].GemsCount.add(_amount);
     }
+    /**
+    * Add Town to Player Town List
+    * @param self Game
+    * @param _town village address to be added
+    */
     function addPlayerVillage(CCModel storage self, address _town) internal {
         self.VillageOwner[_town] = msg.sender;
         self.Players[msg.sender].Towns.push(_town);
--- contracts/CC-modifiers.sol
+++ contracts/CC-modifiers.sol
@@ -4,19 +4,33 @@ import "./CC-library.sol";
 contract CCmodifiers{
     using CClibrary for CClibrary.CCModel;
     CClibrary.CCModel Game;
-
+    //check User is Village owner
     modifier isVillageOwner() {
         require(Game.VillageOwner[msg.sender] == msg.sender,"User is not Village owner");
         _;
     }
+    /**
+    * check User has Gem to pay
+    * @param _amount Gem amount
+    */
     modifier HaveGem(uint256 _amount) {
         require(Game.Players[msg.sender].GemsCount >= _amount,"insufficient balance");
         _;
     }
+    /**
+    * Check given index is  vaild in address array
+    * @param _array address array to be checked
+    * @param _position index of array
+    */
     modifier isArrayIndex(address[] memory _array,uint256 _position) {
         require(_position >= 0 && _position < _array.length,"Invalid Array Index");
         _;
     }
+     /**
+    * Check given index is  vaild by giving array length
+    * @param _length array length to be checked
+    * @param _position index of array
+    */
     modifier isArrayLength(uint256 _length,uint256 _position) {
         require(_position >= 0 && _position < _length,"Invalid Array Length");
         _;
@@ -25,12 +39,27 @@ contract CCmodifiers{
     //     require(_address1 == _address2,"Address not equal");
     //     _;
     // }
+     /**
+    * Check Two address is equal
+    * @param _address1 first address
+    * @param _address2 second address
+    */
     function isSameAddress(address _address1,address _address2) internal pure{
         require(_address1 == _address2,"Address not equal");
     }
+     /**
+    * Check Two address is not equal
+    * @param _address1 first address
+    * @param _address2 second address
+    */
     function isNotSameAddress(address _address1,address _address2) internal pure{
         require(_address1 != _address2,"Address is equal");
     }
+    /**
+    * Check Two uint is equal
+    * @param _value1 first value
+    * @param _value2 second value
+    */
     function isSameValue(uint256 _value1,uint256 _value2) internal pure{
         require(_value1 == _value2,"Value not equal");
     }
